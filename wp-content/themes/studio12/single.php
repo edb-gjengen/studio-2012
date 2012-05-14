730dc747c22411f41692733576e38cfa3516d885
Sjur Hernes <sjurher@ifi.uio.no>
2012-05-15 01:29:05 +0200

    lol, single was div

--- wp-content/themes/studio12/single.php
+++ wp-content/themes/studio12/single.php
@@ -4,7 +4,7 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<div <?php post_class("event") ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class("event") ?> id="post-<?php the_ID(); ?>">
    
    <h2><?php the_title(); ?></h2>
    
