206241ef31254d38e9f74feb17603ccb731b8e69
Sjur Hernes <sjurher@ifi.uio.no>
2012-05-28 02:50:35 +0200

    småsnask

--- wp-content/themes/studio12/sidebar.php
+++ wp-content/themes/studio12/sidebar.php
@@ -2,7 +2,7 @@
 <div id="sidebar-left" class="sidebar transparent">
   
    <a href="<?php echo get_page_by_title('Bli Frivillig!')->guid; ?>">
-	<img id="frivilligknapp" src="<?php echo get_template_directory_uri(); ?>/images/frivilliglol.png" alt="blimed" />
+	<img id="frivilligknapp" src="<?php echo get_template_directory_uri(); ?>/images/knapp_frivillig.png" alt="blimed" onmouseover=" this.src='<?php echo get_template_directory_uri(); ?>/images/knapp_frivillig_mouseover.png'" onmouseout=" this.src='<?php echo get_template_directory_uri(); ?>/images/knapp_frivillig.png'"  />
    </a>
 
    <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('Sidebar Widgets')) : else : ?>
