0850b267b6ec475ca0879fe4dfeb7a0b66de5627
Sjur Hernes <sjurher@ifi.uio.no>
2012-06-06 18:03:59 +0200

    småfiks

---
+++ wp-content/themes/studio12/artister.php
@@ -0,0 +1,52 @@
+/*
+  Template Name: artister
+*/
+
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+<?php get_sidebar("right"); ?>
+<div id="main-content">
+<?php 
+  $type = 'artist';
+$args=array(
+	    'post_type' => $type,
+	    'post_status' => 'publish',
+	    'paged' => $paged,
+	    'ignore_sticky_posts'=> 1
+	    );
+$temp = $wp_query;  // assign orginal query to temp variable for later use   
+$wp_query = null;
+$wp_query = new WP_Query($args); 
+?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<?php get_template_part( 'loop', 'index' );?>
+
+<article <?php post_class("regular") ?> id="post-<?php the_ID(); ?>">
+<div style="height: 210px;">
+  
+  <?php if (has_post_thumbnail()) { echo the_post_thumbnail('restpost'); } ?>
+   <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+   <div class="entry">
+     <?php the_excerpt(); ?>
+   </div>
+  <a href="<?php the_permalink() ?>">
+  <img class="lesmer" src="<?php echo get_template_directory_uri(); ?>/images/knapp_opp_lesmer.png" alt="Les Mer" onmouseover=" this.src='<?php echo get_template_directory_uri(); ?>/images/knapp_ned_lesmer.png'" onmouseout=" this.src='<?php echo get_template_directory_uri(); ?>/images/knapp_opp_lesmer.png'"  /></a>
+</div>
+       <hr />
+</article>
+
+	<?php endwhile; ?>
+
+	<?php else : ?>
+
+		<h2>Not Found</h2>
+
+	<?php endif; ?>
+
+  </div>
+  <?php get_template_part( 'loop', 'index' );?>
+   <?php get_footer(); ?>
+
+
