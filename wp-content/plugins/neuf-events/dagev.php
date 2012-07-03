ae902b2a259b1a8374a62ffafe05b2864728c798
Sjur Hernes <sjurher@ifi.uio.no>
2012-07-03 03:54:16 +0200

    testing

--- wp-content/plugins/neuf-events/dagev.php
+++ wp-content/plugins/neuf-events/dagev.php
@@ -289,7 +289,8 @@ if (!class_exists("NeufEvents")) {
 	    $type  = get_post_meta( $post->ID, 'neuf_events_type',   true);
 	    $artist = get_post_meta( $post->ID, 'neuf_events_artist',  true) ;
 	    if(!isset($_POST['date']) && $date != intval(date("d", $time))){
-	      $html .= '    <tr class="eventnewday"><td>'.$deys[$date - 13].'</td></tr>'; $date = intval(date("d", $time));
+	      $date = intval(date("d", $time));
+	      $html .= '    <tr class="eventnewday"><td>'.$deys[$date - 13].'</td></tr>'; 
 	    }
 	    if(isset($_POST['date']) && $bol){
 	      $html .= '    <tr class="eventnewday"><td>'.$deys[intval($_POST['date']) - 13].'</td></tr>';
