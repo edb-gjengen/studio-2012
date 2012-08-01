c774749f913d2b3d92c58408c44a33c5faee05cf
Sjur Hernes <sjurher@ulrik.uio.no>
2012-08-01 16:24:05 +0200

    Smallfix

--- wp-content/themes/studio12/footer.php
+++ wp-content/themes/studio12/footer.php
@@ -1,7 +1,9 @@
 <div style="clear:both;"></div>
 </div>
+<a href="http://www.studentersamfundet.no">
 <footer id="foot">
    <img id="sponsor" src="<?php echo get_template_directory_uri(); ?>/images/logo_dns.png" />
 </footer>
+</a>
 </body>  
 </html>
